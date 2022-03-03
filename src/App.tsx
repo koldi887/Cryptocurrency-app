87926d54a4935082474ebe81a36bc5420e6b71ef
dmitri_kolisnitsenko <koldi887@gmail.com>
2022-03-03 12:56:32 +0200

    components templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom'
+import {Layout, Typography, Space, Switch} from 'antd'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
+      <div className='navbar'>
+        <Navbar/>
+      </div>
+      <div className='main'>
+        <Layout>
+            <div className='routes'>
+                <Routes>
+                    <Route path='/' element={<Homepage />} />
+                    <Route path='/exchanges' element={<Exchanges />} />
+                    <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                    <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                    <Route path='/news' element={<News />} />
+                </Routes>
+            </div>
+        </Layout>
+      </div>
+        <div className='footer'>
 
+        </div>
     </div>
   );
 }
